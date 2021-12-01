c7d4d48492f4cbc895adff930165f116edd34b54
visusys <futuremotionrecovery@gmail.com>
2021-11-30 22:08:47 -0600

    Refactored Merge-FlattenDirectory.ps1 yet again

--- Public/Merge-FlattenDirectory.ps1
+++ Public/Merge-FlattenDirectory.ps1
@@ -31,14 +31,14 @@
     System.String (SourcePath and DestinationPath)
 
 .OUTPUTS
-    PSCustomObject with Source/Destination/ItemProperties values.
+    System.IO.FileInfo
 
 .NOTES
     Name: Merge-FlattenDirectory
     Author: Visusys
-    Release: 1.0.2
+    Release: 2.0.0
     License: MIT License
-    DateCreated: 2021-11-20
+    DateCreated: 2021-11-30
 
 .LINK
     https://github.com/visusys
@@ -52,121 +52,160 @@ function Merge-FlattenDirectory {
     param (
         [Parameter(Mandatory, Position = 0, ValueFromPipeline)]
         [ValidateScript({
-            if(!(Test-Path -LiteralPath $_)){
-                throw [System.ArgumentException] "Path does not exist." 
-            }
-            if((Test-IsSensitiveWindowsPath -Path $_ -Strict).IsSensitive){
-                throw [System.ArgumentException] "Path supplied is a protected OS directory."
-            }
-            return $true
-        })]
-        [Alias("source","input","i")]
+                if (!(Test-Path -LiteralPath $_)) {
+                    throw [System.ArgumentException] "Path does not exist." 
+                }
+                if ((Test-IsSensitiveWindowsPath -Path $_ -Strict).IsSensitive) {
+                    throw [System.ArgumentException] "Path supplied is a protected OS directory."
+                }
+                return $true
+            })]
+        [Alias("source", "input", "i")]
         [string]
         $SourcePath,
 
         [Parameter(Mandatory = $false, Position = 1, ValueFromPipelineByPropertyName)]
-        [ValidateScript({
-            if(!(Confirm-ValidWindowsPath $_ -Container -Absolute)){
-                throw [System.ArgumentException] "Path is not valid."
-            }
-            if((Test-IsSensitiveWindowsPath -Path $_ -Strict).IsSensitive){
-                throw [System.ArgumentException] "Path supplied is a protected OS directory."
-            }
-            return $true 
-        })]
-        [Alias("destination","dest","output","o")]
+        [Alias("destination", "dest", "output", "o")]
         [string]
-        $DestinationPath,
+        $DestinationPath = $null,
 
         [Parameter(Mandatory=$false)]
-        [ValidateSet(1, 2, 3, 4, 5, IgnoreCase = $true)]
-        [Int32]
-        $Padding = 3
+        [Switch]
+        $Force,
+
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName)]
+        [ValidateSet(1, 2, 3, 4, 5)]
+        [int32]
+        $DuplicatePadding = 2
     )
 
     begin {
-        $SourcePath = $SourcePath.TrimEnd('\')
-        if ($DestinationPath) {
-            $DestinationPath = $DestinationPath.TrimEnd('\')
-        }
 
-        if($SourcePath -and (!($DestinationPath))){
+        # Trim trailing backslashes and initialize a new temporary directory.
+        $SourcePath         = $SourcePath.TrimEnd('\')
+        $DestinationPath    = $DestinationPath.TrimEnd('\')
+        $TempPath           = (New-TempDirectory).FullName
+
+        New-Item -ItemType Directory -Force -Path $TempPath
+        
+        # Escape $SourcePath so we can use wildcards.
+        $Source = [WildcardPattern]::Escape($SourcePath)
+
+        # If there is no $DestinationPath supplied, we can assume the operation is to flatten 
+        # only the SourcePath. Thus, we set the DestinationPath to be the same as the SourcePath.
+        if (!$DestinationPath) {
             $DestinationPath = $SourcePath
+
+            # Since there is no destination supplied, we move everything to a temporary 
+            # directory for further processing.
+            Move-Item -Path $Source'\*' -Destination $TempPath -Force
+
+        }else{
+
+            # We need to perform some parameter validation on DestinationPath:
+
+            # Make sure the passed Destination is not a file
+            if(Test-Path -LiteralPath $DestinationPath -PathType Leaf){
+                throw [System.IO.IOException] "Please provide a valid directory, not a file."
+            }
+
+            # Make sure the passed Destination is a validly formed Windows path.
+            if(!(Confirm-ValidWindowsPath -Path $DestinationPath -Container)){
+                throw [System.IO.IOException] "Invalid Destination Path. Please provide a valid directory."
+            }
+
+            # Make sure the passed Destination is not in a protected or sensitive OS location.
+            if((Test-IsSensitiveWindowsPath -Path $DestinationPath -Strict).IsSensitive){
+                throw [System.IO.IOException] "The destination path is, or resides in a protected operating system directory."
+            }
+
+            # Since a destination was supplied, we copy everything to a new temp directory 
+            # instead of moving everything. We want the source directory to remain untouched.
+
+            # For some reason, Copy-Item is working faster than Robocopy.
+            # Robocopy on Large Dataset:  ~1386ms - ~1400ms
+            # Copy-Item on Large Dataset: ~1203ms - ~1280ms
+            #
+            Copy-Item -Path $Source'\*' -Destination $TempPath -Force -Recurse
+            # Robocopy $Source $TempPath /COPYALL /B /MIR /E /R:0 /W:0 /NFL /NDL /MT:32
+
+            # Create the destination directory now, ready for population in the process block.
+            New-Item -ItemType Directory -Force -Path $DestinationPath
+
+            # Write-Host "`$TempPath:" $TempPath -ForegroundColor Green
         }
 
-        $TempDirObject      = New-TempDirectory
-        $TempDirPath        = $TempDirObject.FullName
+        # Immediately grab all files as an Array of FileInfo Objects
+        $AllFiles = [IO.DirectoryInfo]::new($TempPath).GetFiles('*', 'AllDirectories')
         
-        if(!(Test-Path -LiteralPath $TempDirPath -PathType Container)){
-            New-Item -LiteralPath $TempDirPath -ItemType Container
-        }
-        
-        Copy-Item -LiteralPath $SourcePath -Destination $TempDirPath -Force -Recurse
-
-        $Hash = @{}
-        $AllFiles = [IO.DirectoryInfo]::new($TempDirPath).GetFiles('*', 'AllDirectories')
-        $FileList = [System.Collections.Generic.List[object]]@()
-
     }
 
     process {
+
+        ##
+        # $Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+        # 
+
+        # Initialize array to store duplicate files
+        $Duplicates = @{}
         
+        # Iterate over all files
         foreach ($File in $AllFiles) {
-            $Key = $File.Name
-            # Create Generic List to hold groups, store FileInfo objects in this list
-            # Specialized generic lists are faster than ArrayList
-            if ($Hash.ContainsKey($Key) -eq $false) {
-                $Hash[$Key] = [Collections.Generic.List[System.IO.FileInfo]]::new()
-            }
-            $Hash[$Key].Add($File)
-        }
 
-        foreach ($pile in $Hash.Values) {
-            if ($pile.Count -gt 1) {
-                $i = 1
-                foreach ($pileitem in $pile) {
-                    # $pileitem # System.IO.FileSystemInfo
-                    $fileobj = [PSCustomObject]@{
-                        FilenameOld	   = $pileitem.Name
-                        FilenameNew	   = $pileitem.Name
-                        FileSystemInfo = $pileitem
-                    }
-                    if ($i -gt 1) {
-                        $x = ([string]$i).PadLeft($Padding,'0')
-                        $fileobj.FilenameNew = $pileitem.BaseName + '_' + $x + $pileitem.Extension
-                        $FileList.Add($fileobj)
-                    }else{
-                        $FileList.Add($fileobj)
-                    }
-                    $i += 1
-                }
+            # If our $Duplicates array already contains the current filename, we have a duplicate.
+            if ($Duplicates.Contains($File.Name)) {
+
+                # Rename the duplicate file by appending a numerical index to the end of the file.
+                $PathTemp = Get-ItemProperty -LiteralPath $File
+                $RenamedFile = Rename-Item -LiteralPath $PathTemp.PSPath -PassThru -NewName ('{0}_{1}{2}' -f @(
+                    $File.BaseName
+                    $Duplicates[$File.Name].ToString().PadLeft($DuplicatePadding, '0')
+                    $File.Extension
+                ))
+
+                # Increment the duplicate counter and pass $File down to be moved.
+                $Duplicates[$File.Name]++
+                $File = $RenamedFile
+
+            } else {
+
+                # No duplicates were detected. Add a value of 1 to the duplicates array to represent 
+                # the current file. Pass $File down to be moved.
+                $PathTemp = Get-ItemProperty -LiteralPath $File
+                $Duplicates[$File.Name] = 1
+                $File = $PathTemp
+            }
+
+            # If Force is specified, we don't have to worry about duplicate files,
+            # as the operation will overwrite every file with a duplicate filename
+            if($Force){
+
+                # Move the file to its appropriate destination. (Force)
+                Move-Item -LiteralPath $File -Destination $DestinationPath -Force
             }else{
-                $fileobj = [PSCustomObject]@{
-                    FilenameOld	    = $pile[0].Name
-                    FilenameNew	    = $pile[0].Name
-                    FileSystemInfo  = $pile[0]
+                try {
+
+                    # Move the file to its appropriate destination. (Non-Force)
+                    Move-Item -LiteralPath $File -Destination $DestinationPath -ErrorAction Stop
+                } catch {
+
+                    # Warn the user that files were skipped because of duplicate filenames.
+                    Write-Warning "$($File.Name) already exists in the destination folder. Skipping this file."
                 }
-                $FileList.Add($fileobj)
             }
+
+            # Return each file to the pipeline.
+            $File
         }
 
-        if($SourcePath -eq $DestinationPath){
-            Remove-Item $SourcePath -Recurse
-        }
+        # Write-Host "`$Stopwatch.Stop():" $Stopwatch.Stop() -ForegroundColor Green
+        # Write-Host "`$Stopwatch.ElapsedMilliseconds:" $Stopwatch.ElapsedMilliseconds -ForegroundColor Green 
+        # Write-Host "`$Stopwatch.ElapsedTicks:" $Stopwatch.ElapsedTicks -ForegroundColor Green 
+    }
 
-        if(!(Test-Path -LiteralPath $DestinationPath -PathType Container)){
-            New-Item -Path $DestinationPath -ItemType Container
-        }
-
-        foreach ($Object in $FileList) {
-
-            $ToRename =  [IO.Path]::Combine($Object.FileSystemInfo.DirectoryName, $Object.FilenameOld)
-            $ToMove   =  [IO.Path]::Combine($Object.FileSystemInfo.DirectoryName, $Object.FilenameNew)
-
-            Rename-Item -LiteralPath $ToRename -NewName $Object.FilenameNew
-            Move-Item -LiteralPath $ToMove -Destination $DestinationPath
-        }
+    end {
+        
     }
 }
 
-# Merge-FlattenDirectory "C:\Users\futur\Desktop\Testing\Test"
+# Merge-FlattenDirectory "C:\Users\futur\Desktop\Testing\Test" "C:\Users\futur\Desktop\Testing\TestYey" -Force
