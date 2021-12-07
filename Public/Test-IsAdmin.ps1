df89864212d3e6281e958b7348f3322225ba333a
visusys <futuremotionrecovery@gmail.com>
2021-12-07 06:09:43 -0600

    Housekeeping

--- Public/Test-IsAdmin.ps1
+++
@@ -1,5 +0,0 @@
-function Test-IsAdmin {
-    $Identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-    $Principal = New-Object System.Security.Principal.WindowsPrincipal($Identity)
-    $Principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
-}
--- Public/Test-IsUacEnabled.ps1
+++
@@ -1,3 +0,0 @@
-function Test-IsUacEnabled {
-    (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System).EnableLua -ne 0
-}
